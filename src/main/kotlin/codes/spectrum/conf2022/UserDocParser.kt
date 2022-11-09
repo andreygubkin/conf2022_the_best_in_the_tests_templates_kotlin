f510ad4fcc8b33da0f1a02bce60bacc7f4288e79
Andrey Gubkin <andreygubkin@mail.ru>
2022-11-10 01:30:19 +0500

    пройдены квалификационные тесты

--- src/main/kotlin/codes/spectrum/conf2022/UserDocParser.kt
+++ src/main/kotlin/codes/spectrum/conf2022/UserDocParser.kt
@@ -10,13 +10,13 @@ import kotlin.random.Random
  *
  * контракт один - пустой конструктор и реализация [IDocParser]
  */
-class UserDocParser: IDocParser {
+class UserDocParser : IDocParser {
     override fun parse(input: String): List<ExtractedDocument> {
         /**
          * Это пример чтобы пройти совсем первый базовый тест, хардкод, но понятно API,
          * просто посмотрите preparedSampleTests для примера
          */
-        if(input.startsWith("BASE_SAMPLE1.")) {
+        if (input.startsWith("BASE_SAMPLE1.")) {
             return preparedSampleTests(input)
         }
         /**
@@ -25,7 +25,7 @@ class UserDocParser: IDocParser {
          * надо честно реализовать спеки по DocType.T1 и DocType.T2
          * мы их будем проверять секретными тестами!!!
          */
-        if(input.startsWith("@ ")) {
+        if (input.startsWith("@ ")) {
             return qualificationTests(input)
         }
 
@@ -38,7 +38,53 @@ class UserDocParser: IDocParser {
 
     private fun qualificationTests(input: String): List<ExtractedDocument> {
         //TODO: вот тут надо пройти квалификацию по тестам из base.csv, которые начинаются на `@ BT...`
-        return emptyList()
+
+        val prefix = "@ "
+        val value = input.removePrefix(prefix)
+        val bttGarbageRegex = """[_-]""".toRegex()
+
+        fun String.normalizeAsBtt(): String {
+            return bttGarbageRegex.replace(this, "")
+        }
+
+        val candidates = buildList {
+            val bttCandidate = value.normalizeAsBtt()
+
+            if (bttCandidate.matches(DocType.T1.normaliseRegex)) {
+                val isValid = bttCandidate
+                    .let {
+                        (it.length == 9) xor (it[4] == '5' && it[7] == '7')
+                    }
+
+                add(
+                    element = ExtractedDocument(
+                        docType = DocType.T1,
+                        value = bttCandidate,
+                        isValidSetup = true,
+                        isValid = isValid,
+                    ),
+                )
+            }
+
+            if (bttCandidate.matches(DocType.T2.normaliseRegex)) {
+                val isValid = bttCandidate
+                    .substring(4..7)
+                    .any {
+                        it == '5'
+                    }
+
+                add(
+                    element = ExtractedDocument(
+                        docType = DocType.T2,
+                        value = bttCandidate,
+                        isValidSetup = true,
+                        isValid = isValid,
+                    ),
+                )
+            }
+        }
+
+        return candidates.sortedByDescending { it.isValid }
     }
 
     private fun preparedSampleTests(input: String): List<ExtractedDocument> {
